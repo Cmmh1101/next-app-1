17917eb8d65ef22c86e50392cb041b4d02ff6065
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-24 11:55:22 -0500

    navigating programatically

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
