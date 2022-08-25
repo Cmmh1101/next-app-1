f12e96105516703c93def16bc0a5c945650b83b7
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-24 20:10:49 -0500

    create: event detail

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const EventPage = () => {
   const router = useRouter();
   console.log(router.query);
 
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  console;
+
+  if (!event) {
+    return <p>No Event Found</p>;
+  }
+
   return (
-    <div>
-      <h1>The project of a given event</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
