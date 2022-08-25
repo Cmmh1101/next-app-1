06d48e0a20492569988e2888967a112b664043bf
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-08-24 23:20:07 -0500

    add: search functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventSearch from "../../components/events/EventsSearch";
 
 const EventsPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <>
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
